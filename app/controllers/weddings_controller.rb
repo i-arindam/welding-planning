5d794c05906c06c9e7cffccc0eda1fa85bb3bbee
Arindam Chakraborty <arismart99@gmail.com>
2018-06-09 14:11:14 +0530

    add controller for weddings, add routes & top level actions

---
+++ app/controllers/weddings_controller.rb
@@ -0,0 +1,40 @@
+class WeddingsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def archive
+  end
+
+  def begin
+  end
+
+  def finish
+  end
+
+  def showcase
+  end
+
+  private
+
+  protected
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :weddings do
+    member do
+      post :archive
+      post :begin
+      post :finish
+
+      get :showcase
+    end
+  end
 end
